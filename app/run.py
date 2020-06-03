018941ff2390c0f283362fe513e2118b1458e710
Ricardo <59628818+Rpinto02@users.noreply.github.com>
2020-06-03 15:07:34 -0300

    Add files via upload
    fixed some broken variable links

--- app/run.py
+++ app/run.py
@@ -33,7 +33,7 @@ engine = create_engine('sqlite:///../data/Messages.db')
 df = pd.read_sql_table('Messages', engine)
 
 # load model
-model = joblib.load("../models/train_classifier.pkl")
+model = joblib.load("../model/train_classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
